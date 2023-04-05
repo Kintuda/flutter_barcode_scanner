98370997d55483bf53fc57f99c3469d60f2886b6
Kin Tuda <32006074+Kintuda@users.noreply.github.com>
2023-04-05 15:31:03 -0300

    Update AppConstants.java

--- android/src/main/java/com/amolg/flutterbarcodescanner/constants/AppConstants.java
+++ android/src/main/java/com/amolg/flutterbarcodescanner/constants/AppConstants.java
@@ -1,8 +1,8 @@
 package com.amolg.flutterbarcodescanner.constants;
 
 public interface AppConstants {
-    int BARCODE_RECT_WIDTH = 1100;
-    int BARCODE_RECT_HEIGHT = 300;
+    int BARCODE_RECT_WIDTH = 1000;
+    int BARCODE_RECT_HEIGHT = 250;
     String BARCODE_LINE_COLOR = "#141F00";
     int BARCODE_LINE_WIDTH = 4;
     int BARCODE_FRAMES = 5;
